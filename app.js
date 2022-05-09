e7dc81ff1ae7283360e219f3eccabdb824416320
Mohit Ranjan <mohit80940@gmail.com>
2022-05-09 12:55:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 var targets = document.querySelectorAll(".test .images");
-var tar = document.querySelectorAll(".test2 .images");
 
 const lazyLoad = (t) => {
   const io = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
       if (!entry.isIntersecting) {
+        // console.log(entry);
+        console.log("returned");
         return;
       }
+
       var img = entry.target;
       var src = img.getAttribute("data-src");
 
       img.setAttribute("src", src);
       img.classList.toggle("fade");
-      io.unobserve(t);
-    });
+
+/*       // io.unobserve(t);
+       */    });
   });
   io.observe(t);
 };
@@ -21,9 +24,3 @@ const lazyLoad = (t) => {
 targets.forEach((target) => {
   lazyLoad(target);
 });
-
-tar.forEach((t) => {
-  lazyLoad(t);
-});
-
-console.log("works");
