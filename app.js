f7b112676052612f9d7cc20dcb5fadaaf8635093
Mohit Ranjan <mohit80940@gmail.com>
2022-05-09 07:28:15 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+var targets = document.querySelectorAll(".test .images");
+var tar = document.querySelectorAll(".test2 .images");
+
+const lazyLoad = (t) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+      var img = entry.target;
+      var src = img.getAttribute("data-src");
+
+      img.setAttribute("src", src);
+      img.classList.toggle("fade");
+      io.unobserve(t);
+    });
+  });
+  io.observe(t);
+};
+
+targets.forEach((target) => {
+  lazyLoad(target);
+});
+
+tar.forEach((t) => {
+  lazyLoad(t);
+});
