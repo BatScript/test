7fdbb9ad5a2ca30e605c3c2e9755357d0d829c1e
Mohit Ranjan <mohit80940@gmail.com>
2022-05-09 14:22:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
 var targets = document.querySelectorAll(".test .images");
 
-// const lazyLoad = (t) => {
-//   const io = new IntersectionObserver((entries, observer) => {
-//     entries.forEach((entry) => {
-//       if (!entry.isIntersecting) {
-//         // console.log(entry);
-//         console.log("returned");
-//         return;
-//       }
+const lazyLoad = (t) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        // console.log(entry);
+        console.log("returned");
+        return;
+      }
 
-//       var img = entry.target;
-//       var src = img.getAttribute("data-src");
+      var img = entry.target;
+      var src = img.getAttribute("data-src");
 
-//       img.setAttribute("src", src);
-//       img.classList.toggle("fade");
-//       io.unobserve(t);
+      img.setAttribute("src", src);
+      img.classList.toggle("fade");
+      io.unobserve(t);
 
-//     });
-//   });
-//   io.observe(t);
-// };
+    });
+  });
+  io.observe(t);
+};
 
-// targets.forEach((target) => {
-//   lazyLoad(target);
-// });
-
-targets.forEach((val) => {
-        var src = val.getAttribute("data-src");
-
-      val.setAttribute("src", src);
-})
+targets.forEach((target) => {
+  lazyLoad(target);
+});
